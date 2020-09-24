47281e90209e8a95560a5413b645b6fb3222b1ae
Thomas Nipen <tnipen@gmail.com>
2020-09-24 16:39:59 +0200

    Vector versions of pressure and humidity calculations

--- docs/0.5.0/search/all_f.js
+++ docs/0.5.0/search/all_f.js
@@ -6,7 +6,7 @@ var searchData=
   ['point_2ecpp',['point.cpp',['../point_8cpp.html',1,'']]],
   ['points',['Points',['../classgridpp_1_1Points.html',1,'gridpp::Points'],['../classgridpp_1_1Points.html#acce86e5c6cef176aa1096b65963e3630',1,'gridpp::Points::Points()'],['../classgridpp_1_1Points.html#a8e07d4406288bd97745b2ba9bc87b80a',1,'gridpp::Points::Points(vec lats, vec lons, vec elevs=vec(), vec lafs=vec())'],['../classgridpp_1_1Points.html#a15e2fe7fd626a31abb18d3ac10a1bbba',1,'gridpp::Points::Points(const Points &amp;other)']]],
   ['points_2ecpp',['points.cpp',['../points_8cpp.html',1,'']]],
-  ['pressure',['pressure',['../namespacegridpp.html#ac58b36dc321b2efebfbd9f7e3a4a75a8',1,'gridpp']]],
+  ['pressure',['pressure',['../namespacegridpp.html#ac58b36dc321b2efebfbd9f7e3a4a75a8',1,'gridpp::pressure(float ielev, float oelev, float ipressure, float itemperature=288.15)'],['../namespacegridpp.html#ae1fb8c7012f7bc43f38d3e083da17c57',1,'gridpp::pressure(const vec &amp;ielev, const vec &amp;oelev, const vec &amp;ipressure, const vec &amp;itemperature)']]],
   ['pressure_2ecpp',['pressure.cpp',['../pressure_8cpp.html',1,'']]],
   ['print_5fmatrix',['print_matrix',['../oi__ensi_8cpp.html#a5d2a61f4fc67e049950616d8fe2b73d8',1,'oi_ensi.cpp']]],
   ['print_5fmatrix_3c_20_3a_3acxtype_20_3e',['print_matrix&lt; ::cxtype &gt;',['../oi__ensi_8cpp.html#a58a831c9efd4463a1083f95204be42bb',1,'oi_ensi.cpp']]],
--- docs/0.5.0/search/functions_d.js
+++ docs/0.5.0/search/functions_d.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['point',['Point',['../classgridpp_1_1Point.html#a3552f08a9e3238a9fbdfbe3b9c49ce5a',1,'gridpp::Point']]],
   ['points',['Points',['../classgridpp_1_1Points.html#acce86e5c6cef176aa1096b65963e3630',1,'gridpp::Points::Points()'],['../classgridpp_1_1Points.html#a8e07d4406288bd97745b2ba9bc87b80a',1,'gridpp::Points::Points(vec lats, vec lons, vec elevs=vec(), vec lafs=vec())'],['../classgridpp_1_1Points.html#a15e2fe7fd626a31abb18d3ac10a1bbba',1,'gridpp::Points::Points(const Points &amp;other)']]],
-  ['pressure',['pressure',['../namespacegridpp.html#ac58b36dc321b2efebfbd9f7e3a4a75a8',1,'gridpp']]],
+  ['pressure',['pressure',['../namespacegridpp.html#ac58b36dc321b2efebfbd9f7e3a4a75a8',1,'gridpp::pressure(float ielev, float oelev, float ipressure, float itemperature=288.15)'],['../namespacegridpp.html#ae1fb8c7012f7bc43f38d3e083da17c57',1,'gridpp::pressure(const vec &amp;ielev, const vec &amp;oelev, const vec &amp;ipressure, const vec &amp;itemperature)']]],
   ['print_5fmatrix',['print_matrix',['../oi__ensi_8cpp.html#a5d2a61f4fc67e049950616d8fe2b73d8',1,'oi_ensi.cpp']]],
   ['print_5fmatrix_3c_20_3a_3acxtype_20_3e',['print_matrix&lt; ::cxtype &gt;',['../oi__ensi_8cpp.html#a58a831c9efd4463a1083f95204be42bb',1,'oi_ensi.cpp']]],
   ['print_5fmatrix_3c_20_3a_3amattype_20_3e',['print_matrix&lt; ::mattype &gt;',['../oi__ensi_8cpp.html#aa9c388c88f2a847449380bc6a3d99011',1,'oi_ensi.cpp']]]
