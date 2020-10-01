e2acc23d965f08d33fe76b47f64678029620204a
Thomas Nipen <tnipen@gmail.com>
2020-10-01 21:29:18 +0200

    SWIG test functions

--- docs/0.5.0/search/all_12.js
+++ docs/0.5.0/search/all_12.js
@@ -10,5 +10,6 @@ var searchData=
   ['std',['Std',['../namespacegridpp.html#a232191788fcb9015e7ee56848e8c6e4ea8fe5956039806ec9bd44951678633313',1,'gridpp']]],
   ['structure_2ecpp',['structure.cpp',['../structure_8cpp.html',1,'']]],
   ['structurefunction',['StructureFunction',['../classgridpp_1_1StructureFunction.html',1,'gridpp::StructureFunction'],['../classgridpp_1_1StructureFunction.html#a96c4e6ca99c70e872c8900bedb342a62',1,'gridpp::StructureFunction::StructureFunction()']]],
-  ['sum',['Sum',['../namespacegridpp.html#a232191788fcb9015e7ee56848e8c6e4ea6c441851323ce18fdc0ceab44a5871b9',1,'gridpp']]]
+  ['sum',['Sum',['../namespacegridpp.html#a232191788fcb9015e7ee56848e8c6e4ea6c441851323ce18fdc0ceab44a5871b9',1,'gridpp']]],
+  ['swig_2ecpp',['swig.cpp',['../swig_8cpp.html',1,'']]]
 ];
