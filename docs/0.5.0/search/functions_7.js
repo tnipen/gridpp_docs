bb904e6e0b9afd2ee48a431cb1a5fa015bb3de7b
Thomas Nipen <tnipen@gmail.com>
2020-10-27 19:18:55 +0100

    Transform and cross validation changes

--- docs/0.5.0/search/functions_7.js
+++ docs/0.5.0/search/functions_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['init_5fivec2',['init_ivec2',['../namespacegridpp.html#a836d012b8cd84738991532cadc2338bc',1,'gridpp']]],
-  ['init_5fivec3',['init_ivec3',['../namespacegridpp.html#af0ba1f360e7dcec399bcdb8933bee3bd',1,'gridpp']]],
+  ['init_5fivec2',['init_ivec2',['../namespacegridpp.html#a631bb707c7757265090e384798e7c129',1,'gridpp']]],
+  ['init_5fivec3',['init_ivec3',['../namespacegridpp.html#a0e69a8b88e7116abdb1744580c9ab961',1,'gridpp']]],
   ['init_5fvec2',['init_vec2',['../namespacegridpp.html#ab9d50de69e08ca72dbd4d8ad55cf6ac8',1,'gridpp']]],
   ['init_5fvec3',['init_vec3',['../namespacegridpp.html#a55516a1cc2281b4d56c56317a89f67ba',1,'gridpp']]],
   ['initialize_5fomp',['initialize_omp',['../namespacegridpp.html#a0f1c8ee0e6c4d65abf89ba93378a7c28',1,'gridpp']]],
